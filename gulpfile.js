c5b0548d0722a9ab4ef54a7551fb36b0da186466
Andrew Stacy <astacy@stratacache.com>
2016-10-12 12:55:10 -0400

    Updated gulpfile to generate the proper filename and installed gulp-rename module.

--- gulpfile.js
+++ gulpfile.js
@@ -15,11 +15,13 @@ var gulp = require('gulp');
 
 // Gulp plugins
 var njk = require('gulp-nunjucks');
+var rename = require('gulp-rename');
 
 // Build the Polymer file from the source
 gulp.task('default', function() {
   var fileContent = fs.readFileSync("./bower_components/animate.css/animate.css", "utf8");
   return gulp.src('source/polymer-template.html')
     .pipe(njk.compile({css: fileContent}))
+    .pipe(rename('polymer.animate.css.html'))
     .pipe(gulp.dest('.'));
 });
